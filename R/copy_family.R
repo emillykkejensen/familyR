12a58f7fde98c44a4b35e81b88ffa9625ebf73ea
emillykkejensen <emil@emillykkejensen.dk>
2018-08-27 11:35:29 +0000

    Added copy function

---
+++ R/copy_family.R
@@ -0,0 +1,31 @@
+#' Copy family data
+#'
+#' Copy one list of familyR data to another.
+#'
+#' As familyR relies on data.table it also builds on the principle of changing input
+#' by reference. Therefor you need to specify, that you want to copy data from on data.table
+#' to another insted of just referencing. This is, what the copy_family does.
+#'
+#' @param graph_dt graph_dt; Graph_dt list of data.tables with edges and nodes
+#'
+#' @return A copied version of the inputted graph_dt data.tables.
+#'
+#' @examples
+#' my_network <- data.table::data.table(
+#'   from = c("A", "A", "B", "C", "X", "Y"),
+#'   to = c("B", "C", "D", "E", "Y", "Z")
+#' )
+#'
+#' my_family <- get_familytree(my_network, "B")
+#'
+#' my_copied_family <- copy_family(my_family)
+#'
+#' @export
+copy_family <- function(graph_dt){
+
+  new_nodes <- copy(graph_dt$nodes)
+  new_edges <- copy(graph_dt$edges)
+
+  return(list(nodes = new_nodes, edges = new_edges))
+
+}
